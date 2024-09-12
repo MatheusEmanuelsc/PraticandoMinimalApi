cb0d2c35edfbfd6e8672239317d89867895d7df0
Matheus Emanuel <matheus.emanuelsc@gmail.com>
2024-09-12 16:13:11 -0300

    Api Catalogo inicio

---
+++ ApiCatalogo/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ApiCatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCatalogo.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        DbSet<Categoria>?Categorias { get; set; }
+        DbSet<Produto>? Produtos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
